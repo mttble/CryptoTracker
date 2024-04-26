6850d03ef2510e4af5c212dc175eb6716cc0f624
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-04-26 10:39:54 +0930

    Update dependencies and add @mui/lab package

---
+++ src/components/Dashboard/Tabs/tabs.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import Tab from '@mui/material/Tab';
+import TabContext from '@mui/lab/TabContext';
+import TabList from '@mui/lab/TabList';
+import TabPanel from '@mui/lab/TabPanel';
+import { color } from 'framer-motion';
+import { create } from '@mui/material/styles/createTransitions';
+import { createTheme, ThemeProvider } from '@mui/material';
+
+export default function TabsComponent() {
+  const [value, setValue] = useState('grid');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#3a80e9',
+      },
+    },
+  })
+
+  const style = {
+    color: 'var(--white)',
+    width: "50vw",
+    fontSize: "1.2rem",
+    fontWeight: 600,
+    fontFamily: "Inter",
+    textTransform: "capitalize",
+  }
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <TabContext value={value}>
+          <TabList onChange={handleChange} variant='fullWidth'>
+            <Tab label="Grid" value="grid" sx={style} />
+            <Tab label="List" value="list" sx={style} />
+          </TabList>
+
+        <TabPanel value="grid">
+          <div>mapping for grids</div>
+        </TabPanel>
+        <TabPanel value="list">
+          <div>mapping for lists</div>
+        </TabPanel>
+      </TabContext>
+    </ThemeProvider>
+  );
+}
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Common/Header'
+import TabsComponent from '../components/Dashboard/Tabs/tabs'
 
 function DashboardPage() {
   return (
     <div>
       <Header />
+      <TabsComponent />
     </div>
   )
 }
