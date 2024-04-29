5f7096297dc75e79d5d1577e0fe4420d48c5ac10
mttble <matthew.beale1@hotmail.com>
2024-04-29 14:16:13 +0930

    added more information to grid card components and styled

--- src/components/Dashboard/Grid/index.js
+++ src/components/Dashboard/Grid/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import "./styles.css"
+import TrendingUpRoundedIcon from '@mui/icons-material/TrendingUpRounded';
+import TrendingDownRoundedIcon from '@mui/icons-material/TrendingDownRounded';
 
 function Grid({ coin }) {
   return (
-    <div className='grid-container'>
+    <div className={`grid-container ${coin.price_change_percentage_24h < 0 && "grid-container-red"}`}>
         <div className='info-flex'>
             <img src={coin.image} alt={coin.name} className='coin-logo'/>
             <div className='name-col'>
@@ -11,8 +13,38 @@ function Grid({ coin }) {
                 <p className='coin-name'>{coin.name}</p>
             </div>
         </div>
+        {coin.price_change_percentage_24h > 0 ? (
         <div className='chip-flex'>
-            <div className='price-chip'>{coin.price_change_percentage_24h.toFixed(2)}%</div>
+            <div className='price-chip'>
+                {coin.price_change_percentage_24h.toFixed(2)}%
+            </div>
+            <div className='icon-chip'>
+                <TrendingUpRoundedIcon/>
+            </div>
+        </div>
+        ) : (
+        <div className='chip-flex'>
+            <div className='price-chip chip-red'>
+                {coin.price_change_percentage_24h.toFixed(2)}%
+            </div>
+            <div className='icon-chip icon-chip-red'>
+                <TrendingDownRoundedIcon/>
+            </div>
+        </div>
+        )}
+        <div className='info-container'>
+            <h3 className='coin-price' 
+            style={{
+                color:
+                    coin.price_change_percentage_24h < 0 
+                    ? "var(--red)" 
+                    : "var(--green)"
+                }}
+            >
+                        ${coin.current_price.toLocaleString()}
+            </h3>
+            <p className='total_volume'>Total Volume : {coin.total_volume.toLocaleString()}</p>
+            <p className='total_volume'>Market Cap : {coin.market_cap.toLocaleString()}</p>
         </div>
     </div>
   )
