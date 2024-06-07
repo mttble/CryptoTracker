3d6ded3fde19b678b017d5da4e3e601584ba461d
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-06-07 10:05:08 +0930

    added mobile responsiveness for compare page

--- src/pages/Compare.js
+++ src/pages/Compare.js
@@ -7,6 +7,7 @@ import TextField from '@mui/material/TextField';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import useMediaQuery from '@mui/material/useMediaQuery'
 
 
 function ComparePage() {
@@ -19,6 +20,9 @@ function ComparePage() {
   const [calculatedPrice, setCalculatedPrice] = useState(null);
   const [multiplier, setMultiplier] = useState(null);
   const [showComparisonCard, setShowComparisonCard] = useState(false);
+
+  const isExtraSmallScreen = useMediaQuery('(max-width:600px)');
+  const isSmallScreen = useMediaQuery('(max-width:800px)');
 
   const handleSwitchCoins = () => {
     const tempCoin1 = selectedCoin1;
@@ -77,9 +81,9 @@ function ComparePage() {
     if (coinData) {
       return (
         <div className="comparison-card">
-          <h2>{coinData.name} ({coinData.symbol})</h2>
-          <p>Market Cap: ${coinData.market_cap.toLocaleString()}</p>
-          <p>Price: ${coinData.current_price.toLocaleString()}</p>
+          <h2 style={{fontSize: isSmallScreen ? '1.2rem' : '2rem'}}>{coinData.name} ({coinData.symbol})</h2>
+          <p style={{fontSize: isSmallScreen ? '1rem' : '1.5rem'}}>Market Cap: ${coinData.market_cap.toLocaleString()}</p>
+          <p style={{fontSize: isSmallScreen ? '1rem' : '1.5rem'}}>Price: ${coinData.current_price.toLocaleString()}</p>
         </div>
       );
     } else {
@@ -109,30 +113,42 @@ function ComparePage() {
         <div className="compare-page">
           <h1 style={{ textAlign: 'center', marginBottom: '50px' }}>Compare Cryptocurrencies</h1>
           <Box display="flex" justifyContent="center" gap={2} width="100%">
-            <Autocomplete
-              value={coin1Data}
-              onChange={handleSelectCoin1}
-              options={coins}
-              getOptionLabel={(option) => `${option.name} (${option.symbol})`}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  label="Select Coin 1"
-                  variant="outlined"
-                  inputProps={{
-                    ...params.inputProps,
-                    autoComplete: 'new-password',
-                  }}
-                  sx={{ minWidth: 200, maxWidth: 'calc(50% - 16px)' }}
-                />
-              )}
-            />
+          <Autocomplete
+            value={coin1Data}
+            onChange={handleSelectCoin1}
+            options={coins}
+            getOptionLabel={(option) => `${option.name} (${option.symbol})`}
+            sx={isSmallScreen ? { 
+              width: '80%', 
+              transform: 'scale(0.8)'
+            } : { 
+              width: '250px'
+            }}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                label="Select Coin 1"
+                variant="outlined"
+                inputProps={{
+                  ...params.inputProps,
+                  autoComplete: 'new-password',
+                }}
+                fullWidth
+              />
+            )}
+          />
             <Button variant="contained" onClick={handleSwitchCoins}>Switch</Button>
             <Autocomplete
               value={coin2Data}
               onChange={handleSelectCoin2}
               options={coins}
               getOptionLabel={(option) => `${option.name} (${option.symbol})`}
+              sx={isSmallScreen ? { 
+                width: '80%', 
+                transform: 'scale(0.8)'
+              } : { 
+                width: '250px'
+              }}
               renderInput={(params) => (
                 <TextField
                   {...params}
@@ -142,25 +158,24 @@ function ComparePage() {
                     ...params.inputProps,
                     autoComplete: 'new-password',
                   }}
-                  sx={{ minWidth: 200, maxWidth: 'calc(50% - 16px)' }}
+                  fullWidth
                 />
               )}
             />
           </Box>
           <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
             <div style={{ display: 'flex', marginRight: '20px' }}>
-              <div style={{ marginRight: '20px' }}>{renderComparisonCard(coin1Data)}</div>
-              <div>{renderComparisonCard(coin2Data)}</div>
+              <div style={{ marginRight: '20px', paddingLeft:'4rem' }}>{renderComparisonCard(coin1Data)}</div>
+              <div style={{ marginRight: '20px', paddingLeft:'3rem' }}>{renderComparisonCard(coin2Data)}</div>
             </div>
           </div>
-          {/* New comparison card */}
           {showComparisonCard && (
             <div className="comparison-card" style={{ textAlign: 'center', marginTop: '20px' }}>
-              <h2 style={{color: '#3A80E9'}}>{coin1Data?.name} with the Market Cap of {coin2Data?.name}</h2>
-              <p>Price: ${calculatedPrice?.toLocaleString()} ({multiplier?.toFixed(2)}x)</p>
+              <h2 style={{color: '#3A80E9', fontSize: isExtraSmallScreen ? '1.2rem' : '2rem'}}>{coin1Data?.name} with the Market Cap of {coin2Data?.name}</h2>
+              <p style={{fontSize: isExtraSmallScreen ? '1rem' : '1.5rem'}}>Price: ${calculatedPrice?.toLocaleString()} ({multiplier?.toFixed(2)}x)</p>
             </div>
           )}
-        </div>
+          </div>
       )}
     </ThemeProvider>
   );
