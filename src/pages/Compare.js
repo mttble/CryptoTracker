51d1ffdcf6f8a8fbf270c92c76335d201bdce30d
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-06-07 10:13:21 +0930

    fixed broken css

--- src/pages/Compare.js
+++ src/pages/Compare.js
@@ -165,8 +165,8 @@ function ComparePage() {
           </Box>
           <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
             <div style={{ display: 'flex', marginRight: '20px' }}>
-              <div style={{ marginRight: '20px', paddingLeft:'4rem' }}>{renderComparisonCard(coin1Data)}</div>
-              <div style={{ marginRight: '20px', paddingLeft:'1rem' }}>{renderComparisonCard(coin2Data)}</div>
+              <div style={{ paddingLeft:'4rem' }}>{renderComparisonCard(coin1Data)}</div>
+              <div style={{ marginRight: '20px', paddingLeft:'1.5rem' }}>{renderComparisonCard(coin2Data)}</div>
             </div>
           </div>
           {showComparisonCard && (
