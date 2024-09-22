3855c647712feba6c406e81aaf1f0259450c1bc6
Matthew Beale <129032908+mttble@users.noreply.github.com>
2024-09-23 07:36:44 +0930

    added link to charts when clicking container in list view on dashboard

--- src/components/Dashboard/CryptoList/index.js
+++ src/components/Dashboard/CryptoList/index.js
@@ -13,100 +13,99 @@ import StarOutlineIcon from "@mui/icons-material/StarOutline";
 function CryptoList({ coin, rank }) {
     const watchlist = JSON.parse(localStorage.getItem("watchlist"));
     const [isCoinAdded, setIsCoinAdded] = useState(watchlist?.includes(coin.id));
+
+    const tradingViewUrl = `https://www.tradingview.com/chart/?symbol=BINANCE%3A${coin.symbol}USDT`;
+
     return (
-        <tr className='list-row'>
-            <p className='coin-rank'>{rank}</p>
-            <Tooltip title="Coin Image" placement="top">
-            <td className='td-image'>
-                <img src={coin.image} alt={coin.name} className='coin-logo'/>
-            </td>
-            </Tooltip>
-            <Tooltip title="Coin" placement="top-start">
-            <td>
-                <div className='name-col-list'>
-                    <p className='coin-symbol td-shrink-list'>{coin.symbol}</p>
-                    <p className='coin-name td-shrink-list'>{coin.name}</p>
-                </div>
-            </td>
-            </Tooltip>
-            {coin.price_change_percentage_24h > 0 ? (
-            <Tooltip title="Price Change 24h" placement="top">
-            <td className='chip-flex-list'>
-                <div className='price-chip-list td-shrink-list'>
-                    {coin.price_change_percentage_24h.toFixed(2)}%
-                </div>
-                <div className='icon-chip td-icon'>
-                    <TrendingUpRoundedIcon/>
-                </div>
-            </td>
-            </Tooltip>
-            ) : (
-            <Tooltip title="Price Change 24h" placement="top-start">
-            <td className='chip-flex-list'>
-                <div className='price-chip-list chip-red td-shrink-list'>
-                    {coin.price_change_percentage_24h.toFixed(2)}%
-                </div>
-                <div className='icon-chip icon-chip-red td-icon'>
-                    <TrendingDownRoundedIcon/>
-                </div>
-            </td>
-            </Tooltip>
-            )}
-            <Tooltip title="Current Price" placement="top">
-            <td>
-                <h3 className='coin-price td-center-align td-shrink-list' 
-                style={{
-                    color:
-                        coin.price_change_percentage_24h < 0 
-                        ? "var(--red)" 
-                        : "var(--green)"
+        <a href={tradingViewUrl} target="_blank" rel="noopener noreferrer">
+            <tr className='list-row'>
+                <p className='coin-rank'>{rank}</p>
+                <Tooltip title="Coin Image" placement="top">
+                    <td className='td-image'>
+                        <img src={coin.image} alt={coin.name} className='coin-logo' />
+                    </td>
+                </Tooltip>
+                <Tooltip title="Coin" placement="top-start">
+                    <td>
+                        <div className='name-col-list'>
+                            <p className='coin-symbol td-shrink-list'>{coin.symbol}</p>
+                            <p className='coin-name td-shrink-list'>{coin.name}</p>
+                        </div>
+                    </td>
+                </Tooltip>
+                {coin.price_change_percentage_24h > 0 ? (
+                    <Tooltip title="Price Change 24h" placement="top">
+                        <td className='chip-flex-list'>
+                            <div className='price-chip-list td-shrink-list'>
+                                {coin.price_change_percentage_24h.toFixed(2)}%
+                            </div>
+                            <div className='icon-chip td-icon'>
+                                <TrendingUpRoundedIcon />
+                            </div>
+                        </td>
+                    </Tooltip>
+                ) : (
+                    <Tooltip title="Price Change 24h" placement="top-start">
+                        <td className='chip-flex-list'>
+                            <div className='price-chip-list chip-red td-shrink-list'>
+                                {coin.price_change_percentage_24h.toFixed(2)}%
+                            </div>
+                            <div className='icon-chip icon-chip-red td-icon'>
+                                <TrendingDownRoundedIcon />
+                            </div>
+                        </td>
+                    </Tooltip>
+                )}
+                <Tooltip title="Current Price" placement="top">
+                    <td>
+                        <h3 className='coin-price td-center-align td-shrink-list'
+                            style={{
+                                color: coin.price_change_percentage_24h < 0 ? "var(--red)" : "var(--green)"
+                            }}
+                        >
+                            ${coin.current_price.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 6 })}
+                        </h3>
+                    </td>
+                </Tooltip>
+
+                <Tooltip title="24h Volume" placement="top-end">
+                    <td className='desktop-td-mkt'>
+                        <p className='total_volume td-right-align td-shrink-list'>{coin.total_volume.toLocaleString()}</p>
+                    </td>
+                </Tooltip>
+                <Tooltip title="24h Volume" placement="top-end">
+                    <td className='mobile-td-mkt'>
+                        <p className='total_volume td-right-align td-shrink-list'>{convertNumber(coin.total_volume)}</p>
+                    </td>
+                </Tooltip>
+
+                <Tooltip title="Market Cap" placement="top-end">
+                    <td className='desktop-td-mkt'>
+                        <p className='total_volume td-right-align td-shrink-list'>{coin.market_cap.toLocaleString()}</p>
+                    </td>
+                </Tooltip>
+                <Tooltip title="Market Cap" placement="top-end">
+                    <td className='mobile-td-mkt'>
+                        <p className='total_volume td-right-align td-shrink-list'>{convertNumber(coin.market_cap)}</p>
+                    </td>
+                </Tooltip>
+                <td
+                    className={`watchlist-icon-list ${coin.price_change_percentage_24h < 0 && "watchlist-icon-list-red"}`}
+                    onClick={(e) => {
+                        e.preventDefault();  // Prevent link navigation when clicking the watchlist icon
+                        if (isCoinAdded) {
+                            removeItemToWatchlist(e, coin.id, setIsCoinAdded);
+                        } else {
+                            setIsCoinAdded(true);
+                            saveItemToWatchlist(e, coin.id);
+                        }
                     }}
                 >
-                        ${coin.current_price.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 6 })}
-                </h3>
-            </td>
-            </Tooltip>
-
-            <Tooltip title="24h Volume" placement="top-end">
-            <td className='desktop-td-mkt'>
-                <p className='total_volume td-right-align td-shrink-list'>{coin.total_volume.toLocaleString()}</p>
-            </td>
-            </Tooltip>
-            <Tooltip title="24h Volume" placement="top-end">
-            <td className='mobile-td-mkt'>
-                <p className='total_volume td-right-align td-shrink-list'>{convertNumber(coin.total_volume)}</p>
-            </td>
-            </Tooltip>
-
-            <Tooltip title="Market Cap" placement="top-end">
-            <td className='desktop-td-mkt'>
-                <p className='total_volume td-right-align td-shrink-list'>{coin.market_cap.toLocaleString()}</p>
-            </td>
-            </Tooltip>
-            <Tooltip title="Market Cap" placement="top-end">
-            <td className='mobile-td-mkt'>
-                <p className='total_volume td-right-align td-shrink-list'>{convertNumber(coin.market_cap)}</p>
-            </td>
-            </Tooltip>
-            <td
-            className={`watchlist-icon-list ${
-                coin.price_change_percentage_24h < 0 && "watchlist-icon-list-red"
-            }`}
-            onClick={(e) => {
-                if (isCoinAdded) {
-                // remove coin
-                removeItemToWatchlist(e, coin.id, setIsCoinAdded);
-                } else {
-                setIsCoinAdded(true);
-                saveItemToWatchlist(e, coin.id);
-                }
-            }}
-            >
-            {isCoinAdded ? <StarIcon /> : <StarOutlineIcon />}
-            </td>
-        
-        </tr>
-    )
+                    {isCoinAdded ? <StarIcon /> : <StarOutlineIcon />}
+                </td>
+            </tr>
+        </a>
+    );
 }
 
-export default CryptoList
+export default CryptoList;
